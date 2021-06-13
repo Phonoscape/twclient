b5e2771bd86d44cadf13c397498a2d99d959afa3
Atsushi Abe <phonoscape@gmail.com>
2021-06-13 14:25:38 +0900

    Contentの幅設定位置変更。

--- FormTimeLine.cs
+++ FormTimeLine.cs
@@ -1040,6 +1040,7 @@ namespace twclient
             //    contents.tableLayoutPanel1.RowStyles[i].Height = h;
             //}
 
+            contents.Width = controlListBox1.GetWidthWithoutScrollbar();
             contents.Height = h * 4;
             contents.pictureBox1.Image = bitmap;
             contents.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -1077,7 +1078,6 @@ namespace twclient
             contents.Left = 0;
             contents.Top = 0;
             //contents.Width = controlListBox1.ClientSize.Width;
-            contents.Width = controlListBox1.GetWidthWithoutScrollbar();
 
             //Panel dp = new Panel();
             //dp.Dock = DockStyle.Fill;
