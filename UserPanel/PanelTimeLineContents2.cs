459e0b2c4da3d8721ff810e49ed863d3b1c43ff4
Atsushi Abe <phonoscape@gmail.com>
2021-07-10 18:27:00 +0900

    WebView2での表示実装の準備。

---
+++ UserPanel/PanelTimeLineContents2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twclient.UserPanel
+{
+    public partial class panelTimeLineContents2 : UserControl
+    {
+        public long tweetId { get; set; }
+
+        public panelTimeLineContents2(long id)
+        {
+            InitializeComponent();
+
+            tweetId = id;
+        }
+    }
+}
